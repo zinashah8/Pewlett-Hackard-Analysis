a291e4e8284c7db92ce9d82b556227fca8056fdc
zinashah8 <zina.shah8@gmail.com>
2021-10-03 06:01:05 -0400

    updated schema

--- schema.sql
+++ schema.sql
@@ -40,22 +40,28 @@ CREATE TABLE salaries (
 
 -- Creating table for dept_emp
 CREATE TABLE dept_emp (
-	dept_no VARCHAR(4) NOT NULL, 
-	emp_no INT NOT NULL, 
-	from_date DATE NOT NULL, 
+	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no, dept_no)
 );
 
 -- Creating table for Titles
 CREATE TABLE titles (
-	emp_no INT NOT NULL, 
-	title VARCHAR(40) NOT NULL, 
+	emp_no INT NOT NULL,
+	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
-	to_date DATE NOT NULL, 
-	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
-	PRIMARY KEY (emp_no)
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no, from_date)
 );
 
 SELECT * FROM departments;
+SELECT * FROM employees;
+SELECT * FROM dept_emp;
+SELECT * FROM dept_manager;
+SELECT * FROM salaries;
+SELECT * FROM titles;
